7a55df33911b158470933ec269c17604550983df
Lirido1404 <maxime.prevot1804kz@gmail.com>
2024-02-26 10:52:42 +0100

    tt

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-[#000000] `} >
         <div className="h-screen bg-[#000000] w-[80%] mx-auto">
           <div className="mt-8 ">
             <div className="flex justify-center">
