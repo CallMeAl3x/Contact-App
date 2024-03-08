e43ca9d8d2ccc6f802f8f7518cdbceb593c12cef
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-03-08 13:30:53 +0100

    file uploade

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ import Modal from "./(components)/Modal";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Contact APP",
+  title: "Contact App",
   description: "Projet MMI",
 };
 
