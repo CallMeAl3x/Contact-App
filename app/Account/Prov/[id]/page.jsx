9fd11b046a8059546786a9ed77b5366beb52f2ec
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-03-21 13:25:29 +0100

    +

--- app/Account/Prov/[id]/page.jsx
+++ app/Account/Prov/[id]/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getServerSession } from "next-auth";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 async function page({ params }) {
   const session = await getServerSession(options);
@@ -14,7 +15,8 @@ async function page({ params }) {
       <p>
         {session?.user?.id} <br />
         {session?.user?.name} <br />
-        {session?.user?.email}
+        {session?.user?.email} <br />
+        <img src={session?.user?.image} width={50} height={50} alt="profile picture of Provider" className="rounded-full" />
       </p>
     </div>
   );
