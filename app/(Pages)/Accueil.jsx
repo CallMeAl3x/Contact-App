57e39e69871f89de68c84acd1b7a996540e332c5
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-03-27 13:30:26 +0100

    Update Accueil.jsx

--- app/(Pages)/Accueil.jsx
+++ app/(Pages)/Accueil.jsx
@@ -106,52 +106,51 @@ export default async function Accueil({ searchParams }) {
   );
 
   return (
-    <main className="flex flex-col h-full">
+<main className='flex flex-col h-full'>
       <Sheet>
         <NavBar />
-        <div className="flex mt-8 gap-2 sm:gap-6 mb-4 items-center">
+        <div className='flex mt-8 gap-2 sm:gap-6 mb-4 items-center'>
           <div
             className={`w-[50px] h-[50px] rounded-full ${
               session?.user?.image ? "" : "bg-white"
-            } outlineperso2 flex items-center justify-center`}
-          >
+            } outlineperso2 flex items-center justify-center`}>
             {session?.user?.image ? (
               <img
                 src={session.user.image}
-                alt="User Profile"
+                alt='User Profile'
                 height={50}
                 width={50}
-                className="rounded-full border-[1px] border-white"
+                className='rounded-full border-[1px] border-white'
               />
             ) : (
               <>
                 <img
                   src={"/avatar2.svg"}
-                  alt=""
-                  className="rounded-full w-full h-full p-2 border-[1px] border-white"
+                  alt=''
+                  className='rounded-full w-full h-full p-2 border-[1px] border-white'
                 />
               </>
             )}
           </div>
-          <div className="my-auto ml-2 flex flex-col gap-1">
-            <h1 className="truncate w-fit font-Jost font-bold text-base">
-              {session?.user?.name} (Me) :
+          <div className='my-auto ml-2 flex flex-col gap-1'>
+            <h1 className='font-Jost font-bold text-base w-full overflow-hidden text-ellipsis whitespace-nowrap'>
+              {session?.user?.name} 
             </h1>
-            <p className="text-[15px] text-gray font-Jost font-semibold">
+            <p className='text-[15px] text-gray font-Jost font-semibold'>
               {session ? (
                 <>{session.user.email}</>
               ) : (
                 <>Vous nêtes pas connecté</>
               )}
             </p>
-            <div className="flex gap-2 text-xs">
-              <p className="">
+            <div className='flex gap-2 text-xs'>
+              <p className=''>
                 {contactsToShow.length > 1
                   ? `${contactsToShow.length} Contacts`
                   : `${contactsToShow.length} Contact`}
               </p>
-              <span className="mt-0.5">°</span>
-              <p className="text-green font-medium">
+              <span className='mt-0.5'>°</span>
+              <p className='text-green font-medium'>
                 {favoriteContacts2.length > 1
                   ? `${favoriteContacts2.length} Favoris`
                   : `${favoriteContacts2.length} Favori`}
@@ -166,148 +165,153 @@ export default async function Accueil({ searchParams }) {
                 ? `/Account/Cred/${session?.user?.id}`
                 : "/api/auth/signin"
             }
-            className="ml-auto flex items-center"
-          >
-            <Image src={chevron} width={30} color="#8D8C8F" alt="chevron" />
+            className='ml-auto flex items-center'>
+            <Image src={chevron} width={30} color='#8D8C8F' alt='chevron' />
           </Link>
         </div>
 
         {!query && (
           <>
-          {sortedAndFilteredContacts2.length > 0 && session && (
-          <>
-            <h2 className="text-5xl text-white font-bold mt-6">
-              Ajouts Récents
-            </h2>
+            {sortedAndFilteredContacts2.length > 0 && session && (
+              <>
+                <h2 className='text-4xl text-white font-bold mt-6'>
+                  Récents
+                </h2>
 
-            <ScrollArea className="whitespace-nowrap rounded-md mt-4">
-              <div className="flex w-max space-x-4 ">
-                {sortedAndFilteredContacts2.map((contact, index) => (
-                  <div key={contact._id} className="">
-                    {!contact.sameAsPrevious && (
-                      <>
-                        <Link
-                          href={`/ContactPage/${contact._id}`}
-                          className="hover:shadow"
-                        >
-                          <Card className="bg-[#303034]">
-                            <CardHeader>
-                              <div className="flex justify-between  items-center rounded-lg gap-4">
-                                <CardTitle className="text-white text-xl">
-                                  {contact.prenom} {contact.nom}
-                                </CardTitle>
-                                {contact.image ? (
-                                  <>
-                                    <img
-                                      src={contact.image}
-                                      alt="pp"
-                                      className="rounded-full h-12 w-12 border-[1px] border-white object-cover"
-                                    />
-                                  </>
-                                ) : (
-                                  <>
-                                    <div className="bg-[#1C1C1E] rounded-full border-[1px] border-white h-12 w-12 flex justify-center items-center">
-                                      <p className="font-Jost font-bold text-white text-[16px]">
-                                        {contact.prenom
-                                          ? contact.prenom[0].toUpperCase()
-                                          : "?"}
-                                      </p>
-                                    </div>
-                                  </>
-                                )}
-                              </div>
-                            </CardHeader>
-                            <CardContent>
-                              <Separator />
-                              <div className="mt-4">
-                                <p className="text-base text-white italic">
-                                  {contact.tel &&
-                                    contact.tel
-                                      .split("")
-                                      .map((char, index) => {
-                                        return index % 2 === 0 &&
-                                          index !== 0 &&
-                                          index !== contact.tel.length - 1
-                                          ? ` ${char}`
-                                          : char;
-                                      })
-                                      .join("")}
-                                </p>
-                                <p className="text-xs text-white italic">
-                                  {contact.email}
-                                </p>
-                              </div>
-                              <div className="flex justify-start mt-4">
-                                <Badge variant="secondary">
-                                  <p className="text-xs">
+                <ScrollArea className='whitespace-nowrap rounded-md mt-4'>
+                  <div className='flex w-max space-x-4'>
+                    {sortedAndFilteredContacts2.map((contact, index) => (
+                      <div key={contact._id} className=''>
+                        {!contact.sameAsPrevious && (
+                          <>
+                            <Link
+                              href={`/ContactPage/${contact._id}`}
+                              className='hover:shadow'>
+                              <div className='bg-[#303034] rounded-[14px] p-5 pt-4 pb-0 min-w-[130px] max-w-[155px] xl:max-w-[240px] xl:min-w-[180px]'>
+                                <div className='flex justify-between  items-center rounded-lg gap-4'>
+                                  {contact.image ? (
+                                    <>
+                                      <img
+                                        src={contact.image}
+                                        alt='pp'
+                                        className='rounded-full h-[38px] w-[38px] xl:h-[50px] xl:w-[50px] border-[1px] border-white object-cover'
+                                      />
+                                    </>
+                                  ) : (
+                                    <>
+                                      <div className='bg-[#1C1C1E] rounded-full border-[1px] border-white h-[38px] w-[38px] xl:h-[50px] xl:w-[50px] flex justify-center items-center'>
+                                        <p className='font-Jost font-bold text-white text-[16px]'>
+                                          {contact.prenom
+                                            ? contact.prenom[0].toUpperCase()
+                                            : "?"}
+                                        </p>
+                                      </div>
+                                    </>
+                                  )}
+                                </div>
+                                <div className='text-white text-[15px] font-Jost font-semibold mt-3 flex items-center w-full overflow-hidden text-ellipsis whitespace-nowrap'>
+                                <p className="mr-2">{contact.prenom.charAt(0).toUpperCase() + contact.prenom.slice(1).toLowerCase()}</p>
+                                <p>{contact.nom.charAt(0).toUpperCase() + contact.nom.slice(1).toLowerCase()}</p>
+                                </div>
+
+                                <div className='mt-0.5'>
+                                  <p className='text-[12px] text-[#EBEBF5] text-opacity-50'>
+                                    {contact.tel &&
+                                      contact.tel
+                                        .split("")
+                                        .map((char, index) => {
+                                          return index % 2 === 0 &&
+                                            index !== 0 &&
+                                            index !== contact.tel.length - 1
+                                            ? ` ${char}`
+                                            : char;
+                                        })
+                                        .join("")}
+                                  </p>
+                                </div>
+                                <div className='flex gap-3 pb-4 mt-2'>
+                                  <Image
+                                    src={"tel.svg"}
+                                    alt='tel'
+                                    width={35}
+                                    height={35}
+                                    className='flex-shrink-0'
+                                  />
+                                  <Image
+                                    src={"message.svg"}
+                                    alt='mail'
+                                    width={35}
+                                    height={35}
+                                    className='flex-shrink-0'
+                                  />
+                                </div>
+                                <p className="text-xs hidden">
                                     {new Date(
                                       contact.createdAt
                                     ).toLocaleDateString()}
                                   </p>
-                                </Badge>
                               </div>
-                            </CardContent>
-                          </Card>
-                        </Link>
-                      </>
-                    )}
+                            </Link>
+                          </>
+                        )}
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-              <ScrollBar orientation="horizontal" />
-            </ScrollArea>
-          </>
-        )}
+                  <ScrollBar orientation='horizontal' />
+                </ScrollArea>
+              </>
+            )}
           </>
         )}
 
         {sortedAndFilteredContacts2.length > 0 && session && (
           <>
-            <h2 className="text-5xl text-white font-bold mt-10">Contacts</h2>
+            <h2 className='text-4xl text-white font-bold mt-10'>Contacts</h2>
           </>
         )}
 
-        <div className="mt-2">
+        <div className='mt-2'>
           <MappingContact
             contactsToShow={contactsToShow}
             favoriteContacts2={favoriteContacts2}
+            query={query}
           />
         </div>
 
-        <div className="ml-auto mr-auto mt-32">
+        <div className='ml-auto mr-auto mt-32'>
           <SheetTrigger>
             {!session && (
               <>
-                <div className="">
-                  <Image src={box} alt="box" width={170} height={170} />
+                <div className=''>
+                  <Image src={box} alt='box' width={170} height={170} />
                 </div>
               </>
             )}
             {!contactsToShow.length > 0 && session && (
               <>
-                <Link className="" href={"/ContactPage/new"}>
-                  <Image src={box} alt="box" width={170} height={170} />
+                <Link className='' href={"/ContactPage/new"}>
+                  <Image src={box} alt='box' width={170} height={170} />
                 </Link>
               </>
             )}
           </SheetTrigger>
         </div>
 
-        <SheetContent className="bg-primary">
+        <SheetContent className='bg-primary'>
           <SheetHeader>
             <SheetTitle>
               {session ? (
                 <>
-                  <p className="text-white font-bold">Déconnectez-vous</p>
+                  <p className='text-white font-bold'>Déconnectez-vous</p>
                 </>
               ) : (
                 <>
-                  <p className="text-white font-bold">Connectez-vous avec :</p>
+                  <p className='text-white font-bold'>Connectez-vous avec :</p>
                 </>
               )}
             </SheetTitle>
             <SheetDescription>
-              <div className="mt-4">
+              <div className='mt-4'>
                 {session ? (
                   <>
                     <Exit />
